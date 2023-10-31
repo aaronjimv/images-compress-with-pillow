8dda87f9a22d4a726ecfd0548fbde17923635423
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-30 18:48:38 -0800

    compress logict created

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ def compress_images(image_folder):
     try:
         for file in os.listdir(image_folder):
             file_name, file_extension = os.path.splitext(image_folder + file)
-            print("compressing images " + file_name)
-    
-    except:
-        print("ERROR!!!")
+            print("compressing images " + file_name + file_extension)
+
+            if file_extension == '.jpg':
+                file_compress = Image.open(image_folder + file)
+                file_compress.save(
+                    file_name + "_compress.jpg",
+                    optimize=True,
+                    quality=20
+                )
+    except FileNotFoundError as error:
+        print(f"ERROR: {error}")
 
 
 if __name__ == '__main__':
